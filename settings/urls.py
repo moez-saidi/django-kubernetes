b9d156efd3a23629f90552f628ae5f525a859159
Moez Saidi <saidi.moez.2014@gmail.com>
2023-01-26 14:28:10 +0100

    Create new app for cluster management

--- settings/urls.py
+++ settings/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from settings.utils import ENVS
 
 urlpatterns = [
     path(f'{settings.API_PREFIX}/admin/', admin.site.urls),
+    path(f'{settings.API_PREFIX}/cluster/', include('cluster_management.urls')),
 ]
 
 if settings.ENV == ENVS.DEV:
